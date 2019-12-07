d6bc22e7328b83c098c4053095995665284e7aa3
GooGee <mou-gj@live.com>
2019-12-08 00:27:19 +0800

    - PropertyManager in FileType

--- src/components/FileType.vue
+++ src/components/FileType.vue
@@ -6,52 +6,48 @@
         </div>
 
         <div v-for="file in bus.project.FileTypeManager.list" :key="file.name">
-            <template v-if="bus.item && file.name==bus.item.name">
-                <table class="table">
-                    <caption class="title">
-                        <h1>{{ file.name }}</h1>
-                        <span v-if="!file.original" @click="remove(file)" class="btn btn-danger"> X </span>
-                    </caption>
-                    <thead>
-                        <tr>
-                            <th width="160px">Name</th>
-                            <th>Value</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>path</td>
-                            <td>
-                                <span @click="change('path', file)" class="btn btn-default">{{ plus(file.path) }}</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>NameSpace</td>
-                            <td>
-                                <span @click="change('NameSpace', file)" class="btn btn-default">{{ plus(file.NameSpace) }}</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>prefix</td>
-                            <td>
-                                <span @click="change('prefix', file)" class="btn btn-default">{{ plus(file.prefix) }}</span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>suffix</td>
-                            <td>
-                                <span @click="change('suffix', file)" class="btn btn-default">{{ plus(file.suffix) }}</span>
-                            </td>
-                        </tr>
-                        <tr v-show="visible">
-                            <td></td>
-                            <td><textarea v-model="file.template" rows="11" class="form-control"></textarea></td>
-                        </tr>
-                    </tbody>
-                </table>
-
-                <Property :manager="file.PropertyManager"></Property>
-            </template>
+            <table v-if="bus.item && file.name==bus.item.name" class="table">
+                <caption class="title">
+                    <h1>{{ file.name }}</h1>
+                    <span v-if="!file.original" @click="remove(file)" class="btn btn-danger"> X </span>
+                </caption>
+                <thead>
+                    <tr>
+                        <th width="160px">Name</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>path</td>
+                        <td>
+                            <span @click="change('path', file)" class="btn btn-default">{{ plus(file.path) }}</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>NameSpace</td>
+                        <td>
+                            <span @click="change('NameSpace', file)" class="btn btn-default">{{ plus(file.NameSpace) }}</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>prefix</td>
+                        <td>
+                            <span @click="change('prefix', file)" class="btn btn-default">{{ plus(file.prefix) }}</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>suffix</td>
+                        <td>
+                            <span @click="change('suffix', file)" class="btn btn-default">{{ plus(file.suffix) }}</span>
+                        </td>
+                    </tr>
+                    <tr v-show="visible">
+                        <td></td>
+                        <td><textarea v-model="file.template" rows="11" class="form-control"></textarea></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
@@ -59,11 +55,9 @@
 <script>
     import bus from '../helper/event';
     import { see, sure, enter } from '../helper/dialogue';
-    import Property from './Property';
 
     export default {
         name: 'FileType',
-        components: { Property },
         data() {
             return {
                 bus,
